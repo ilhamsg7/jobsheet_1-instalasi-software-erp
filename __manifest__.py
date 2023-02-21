9f1576de8c3f2b1329567426d2a27a515d8a1fed
ilhamsg7 <sinatrio20@gmail.com>
2023-02-21 08:55:22 +0700

    initial commit

---
+++ __manifest__.py
@@ -0,0 +1,23 @@
+{
+    "name"          : "New Addons",
+    "version"       : "1.0",
+    "author"        : "Ilham Sinatrio Gumelar",
+    "website"       : "https://ilham-sg.my.id",
+    "category"      : "New Module",
+    "summary"       : "Ini adalah addons baru",
+    "description"   : """
+        Latihan membuat addons baru di Odoo
+    """,
+    "depends"       : [
+        "base",
+    ],
+    "data"          : [],
+    "demo"          : [],
+    "test"          : [],
+    "image"         : [],
+    "qweb"          : [],
+    "css"           : [],
+    "application"   : True,
+    "installable"   : True,
+    "auto_install"  : False,
+}
